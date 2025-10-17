792dc329275e132e8dde89e58b765ef025ff9d87
maxzorTEA23 <maximilianzorko@gmail.com>
2025-10-17 15:05:41 +0200

    added solution

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -2,7 +2,10 @@
 #include <fmt/format.h>
 
 #include "config.h"
-
+int globalVar = 1;
+const int globalVar2 = 0;
+static int globalVar3;
+void foo();
 int main(int argc, char **argv) {
 
     /**
@@ -10,9 +13,23 @@ int main(int argc, char **argv) {
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
-
+    /*Übung 2*/
+    fmt::print("{}, Adresse: {} \n", globalVar, fmt::ptr(&globalVar));
+    fmt::print("{}, Adresse: {} \n", globalVar2, fmt::ptr(&globalVar2));
+    fmt::print("{}, Adresse: {} \n", globalVar3, fmt::ptr(&globalVar3));
+    int localVar = 2;
+    static int staticVar = 3;
+    fmt::print("{}, Adresse: {} \n", localVar, fmt::ptr(&localVar));
+    fmt::print("{}, Adresse: {} \n", staticVar, fmt::ptr(&staticVar));
+    /*Übung 3*/
+    int*  heapVar = new int(4);
+    fmt::print("{}, Adresse: {} \n", *heapVar, fmt::ptr(heapVar));
+    /*Übung4*/
+    fmt::print("Adresse von foo: {}\\n", fmt::ptr(&foo));
     /* INSERT YOUR CODE HERE */
 
     return 0; /* exit gracefully*/
+}
+void foo(){
+    { fmt::print("Hello from foo!\\n"); }
 }
